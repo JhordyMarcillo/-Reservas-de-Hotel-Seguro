2a61e3bd14e3449d65a78ca21db5acb0344bf4be
JhordyMarcillo <jhordypaulsb2@gmail.com>
2026-04-14 12:42:03 -0500

    first commit

---
+++ src/test/java/seguro/seguro/SeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package seguro.seguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
